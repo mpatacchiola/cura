3014e5cff9e634595e0bfdddcf59b376b2706687
Tayal <357677@CTS.COM>
2016-11-20 04:11:08 +0100

    code added to parse number string to digit

--- src/parser.py
+++ src/parser.py
@@ -47,4 +47,76 @@ class Parser:
 
 		return(numberList)			
 		
+	# to convert number text to integer number
+	def text2int (textnum, numwords={}):
+		if not numwords:
+			
+				units = [
+				"zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+				"nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+				"sixteen", "seventeen", "eighteen", "nineteen",
+				]
 
+				tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+				scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+				numwords["and"] = (1, 0)
+				
+				for idx, word in enumerate(units):  numwords[word] = (1, idx)
+				for idx, word in enumerate(tens):       numwords[word] = (1, idx * 10)
+				for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
+				ordinal_words = {'first':1, 'second':2, 'third':3,'for':4,'forth':4, 'fifth':5, 'eighth':8, 'ninth':9, 'twelfth':12}
+				ordinal_endings = [('ieth', 'y'), ('th', '')]
+				
+				textnum = textnum.replace('-', ' ')
+
+				current = result = 0
+				curstring = ""
+				onnumber = False
+				for word in textnum.split():
+						if word in ordinal_words:
+								scale, increment = (1, ordinal_words[word])
+								current = current * scale + increment
+								if scale > 100:
+										result += current
+										current = 0
+								onnumber = True
+						else:
+								for ending, replacement in ordinal_endings:
+										if word.endswith(ending):
+												word = "%s%s" % (word[:-len(ending)], replacement)
+
+								if word not in numwords:
+										if onnumber:
+												curstring += repr(result + current) + " "
+										curstring += word + " "
+										result = current = 0
+										onnumber = False
+								else:
+										scale, increment = numwords[word]
+
+										current = current * scale + increment
+										if scale > 100:
+												result += current
+												current = 0
+										onnumber = True
+
+				if onnumber:
+						curstring += repr(result + current)
+
+				return curstring
+
+				
+# Condition to check the digit				
+#numbers = extractNumbersFromText(audio_data)
+#print("--- Length of number list---------")
+
+#if(len(numbers) == 0):
+	#outputDigit = -1
+#elif len(numbers) == 1:
+	#print(text2int(numbers[0]))
+#	outputDigit = text2int(numbers[0])
+#else:
+#	outputDigit = 0;
+#print(outputDigit)
