31f2244338fe6d5bee44599f4a7876343ce068fe
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-11-20 01:40:48 +0000

    Reorganising the folders

--- src/parser.py
+++ src/parser.py
@@ -16,11 +16,20 @@ class Parser:
 		# Create SpeechToText service object
 		stt = SpeechToTextV1(username=username, password=password)
 		audio_file = open(audioFile, "rb")
-		audio_data = json.dumps(stt.recognize(audio_file, content_type="audio/wav"), indent=2)
-		audio_json_data = json.loads(audio_data)
-		print(" Here is the audio text :::")
-		audio_text = audio_json_data["results"][0]["alternatives"][0]["transcript"]
-		return(audio_text)
+		audio_data = json.dumps(stt.recognize(audio_file, content_type="audio/wav", model='en-US_NarrowbandModel', continuous=True), indent=2)
+		print(audio_data)
+		audio_text=""
+                try:
+		    audio_json_data = json.loads(audio_data)
+		    print(" Here is the audio text :::")
+		    if(len(audio_json_data["results"])!= 0 ):
+		    	audio_text = audio_json_data["results"][0]["alternatives"][0]["transcript"]
+			return(audio_text)
+		    else:
+			return(None)
+		    
+                except Exception,e:
+                    print "Error was: ",e
 		
 		
 	def extractNumbersFromText(self,audioText):
@@ -31,22 +40,11 @@ class Parser:
 		tagged_words = [nltk.pos_tag(word) for word in tokenized_words]
 		for tag in tagged_words:
 			tagged_word = tag[0]
-			if tagged_word[1] == 'CD' or tagged_word[1] == 'RB' or tagged_word[1] == 'NN':
+			if tagged_word[1] == 'CD' or tagged_word[1] == 'RB' or tagged_word[1] == 'NN' or tagged_word[1] == 'JJ':
 				if tagged_word[0] in num_dict:
 					print(tagged_word[0])
 					numberList.append(tagged_word[0])
 
 		return(numberList)			
 		
-# these path are used as example		
-audioFile = " path of the audio file "
-configFile = "path of the config file"
-
-if __name__ == '__main__':
-	obj = Parser(configFile)
-	#audio_data = obj.convertSpeechToText(audioFile)	
-	#audio_data= "is there a zero somewhere"
-	#print(audio_data)
-	#numbers = obj.extractNumbersFromText(audio_data)
-	#print(numbers)
 
